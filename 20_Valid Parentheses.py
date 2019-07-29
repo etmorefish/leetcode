6de64fc0c23583876a93dd893b11ab5e7aba4dca
maolei <849078367@qq.com>
2019-07-29 15:48:28 +0800

    20.有效的括号

---
+++ 20_Valid Parentheses.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19-7-29 下午3:11
+# @Author  : MaoLei
+# @Email   : maolei025@qq.com
+# @File    : 20_Valid Parentheses.py
+# @Software: PyCharm
+
+class Solution:
+    def isValid(self, s):
+        while '{}' in s or '()' in s or '[]' in s:
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+            s = s.replace('()', '')
+        return s == ''
+
+
+'''
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+if __name__ == '__main__':
+    a = Solution()
+    a.isValid('{}')
